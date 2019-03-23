f6f776df2da79f91decdd06f217e341dab69ff3f
Shawn Boudreau <46534718+Sboudreau5@users.noreply.github.com>
2019-03-23 01:00:11 -0400

    Add files via upload

--- js/Hamburgermenu.js
+++ js/Hamburgermenu.js
@@ -7,13 +7,16 @@ let toggleNav= function() {
    let getDropdownUl = document.querySelector(".drop-down ul");
 	let getDropdownLinks = document.querySelectorAll(".drop-down a");
 	let getDropdownBars = document.querySelectorAll("hr");
+	let getExitButton = document.querySelector(".exitmenu");
+	let getHamburgerMenu = document.querySelector(".hamburgermenu");
 
 if (toggleNavStatus === false) {
 	
 	getDropdownUl.style.visibility = "visible";
 	getDropdown.style.opacity = "1";
 	getDropdownUl.style.opacity = "1";
-    
+	getExitButton.style.display = "block";
+    getHamburgerMenu.style.display = "none";
 	
 	let arrayLength = getDropdownLinks.length;
 	
@@ -39,6 +42,8 @@ else if (toggleNavStatus === true) {
 
 	getDropdown.style.opacity = "0";
 	getDropdownUl.style.opacity = "0";
+	getExitButton.style.display = "none";
+    getHamburgerMenu.style.display = "block";
 	
 	
 	let arrayLength = getDropdownLinks.length;
