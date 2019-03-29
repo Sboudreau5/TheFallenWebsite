17bd7cad14c560046b8dc5bb49638700b7d2d9f8
Shawn Boudreau <46534718+Sboudreau5@users.noreply.github.com>
2019-03-29 19:22:32 -0400

    Add files via upload

---
+++ js/Modal.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+
+document.getElementById('andres-photo').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal').style.display = 'flex'							   
+});
+
+
+document.querySelector('.close').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal').style.display = 'none'							   
+});
+
+
