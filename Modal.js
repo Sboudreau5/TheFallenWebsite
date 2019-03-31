4a2216d99f4ddda68c826e8ff89a8390f1228b1d
Shawn Boudreau <46534718+Sboudreau5@users.noreply.github.com>
2019-03-30 21:03:05 -0400

    Add files via upload

--- Modal.js
+++ Modal.js
@@ -13,3 +13,50 @@ document.querySelector('.close').addEventListener('click',
 });
 
 
+document.getElementById('duncan-photo').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal2').style.display = 'flex'							   
+});
+
+
+document.querySelector('.close2').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal2').style.display = 'none'							   
+});
+
+document.getElementById('emma-photo').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal3').style.display = 'flex'							   
+});
+
+
+document.querySelector('.close3').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal3').style.display = 'none'							   
+});
+
+document.getElementById('andrew-photo').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal4').style.display = 'flex'							   
+});
+
+
+document.querySelector('.close4').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal4').style.display = 'none'							   
+});
+
+document.getElementById('phil-photo').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal5').style.display = 'flex'							   
+});
+
+
+document.querySelector('.close5').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal5').style.display = 'none'							   
+});
+
+
+
+
